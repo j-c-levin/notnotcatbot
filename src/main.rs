bd73d27632774c3e0405358f560c147183b6ae21
Joshua Jo'Sai-Levin <9204074+j-c-levin@users.noreply.github.com>
2023-12-21 08:42:42 +0000

    make it neater

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::env;
 use std::error::Error;
 
-use reqwest::{Client, Url};
 use reqwest::header::{HeaderMap, HeaderValue};
+use reqwest::{Client, Url};
 use serde::Deserialize;
-use teloxide::{prelude::*, utils::command::BotCommands};
 use teloxide::repls::CommandReplExt;
 use teloxide::types::InputFile;
+use teloxide::{prelude::*, utils::command::BotCommands};
 
 #[derive(Debug, Deserialize)]
 struct CatApiResponse {
@@ -31,56 +31,47 @@ async fn get_cat_image_url() -> Result<Url, Box<dyn Error>> {
         .await?
         .json::<Vec<CatApiResponse>>()
         .await?;
-// Assuming response.first() returns a Result type
+
     let url_string_result = match response.first() {
         None => Err(Box::new("cannot get response")),
         Some(response) => Ok(response.url.clone()),
     };
 
     let parsed_url_result = match url_string_result {
-        Ok(url_string) => {
-            // Use the Url::parse method and handle its Result
-            match Url::parse(&url_string) {
-                Ok(url) => Ok(url),
-                Err(e) => Err(Box::new(format!("cannot parse url string: {}", e))),
-            }
-        }
-        Err(e) => Err(Box::new(format!("{}", e)))
+        Ok(url_string) => match Url::parse(&url_string) {
+            Ok(url) => Ok(url),
+            Err(e) => Err(Box::new(format!("cannot parse url string: {}", e))),
+        },
+        Err(e) => Err(Box::new(format!("{}", e))),
     };
 
     // Now you have the final Result<Url, Box<dyn std::error::Error>>
     match parsed_url_result {
         Ok(parsed_url) => {
-            // Successfully parsed URL
+            println!("serving image {}", parsed_url);
+
             Ok(parsed_url)
         }
-        Err(e) => Err(Box::from(format!("{}", &e)))
+        Err(e) => Err(Box::from(format!("{}", &e))),
     }
-    // match response.first() {
-    //     None => { Err(Box::from("No response")) }
-    //     Some(response) => {
-    //         println!("serving image {:?}", response.url);
-    //         match response.url.clone().parse::<Url>() {
-    //             Err(_) => { Err(Box::from(format!("Cannot parse url {}", response.url))) }
-    //             Ok(url) => { Ok(url) }
-    //         }
-    //     }
-    // }.expect("TODO: panic message");
-    // Ok(response.first().unwrap().url.clone().parse().unwrap())
 }
 
 async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
     match cmd {
         Command::Cat => {
             let image = get_cat_image_url().await;
-            bot.send_photo(msg.chat.id, InputFile::url(image.unwrap())).await?;
+            bot.send_photo(msg.chat.id, InputFile::url(image.unwrap()))
+                .await?;
         }
     };
     Ok(())
 }
 
 #[derive(BotCommands, Clone)]
-#[command(rename_rule = "lowercase", description = "These commands are supported:")]
+#[command(
+    rename_rule = "lowercase",
+    description = "These commands are supported:"
+)]
 enum Command {
     Cat,
 }
